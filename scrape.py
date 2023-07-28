b1e81ff5a0303f7cacebd4d6aa915e6b5598ca80
Angela Yang <43125640+angela-yang@users.noreply.github.com>
2023-07-27 17:47:41 -0700

    Add files via upload

---
+++ scrape.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+from requests.auth import HTTPDigestAuth
+
+URL = "https://tds.ms/CentralizeSP/BtwScheduling/Lessons?SchedulingTypeId=1"
+#page = requests.get(URL, auth=HTTPDigestAuth('user', 'pass'))
+#page = requests.get(URL, auth=('user', 'pass'))
+page = requests.get(URL)
+
+soup = BeautifulSoup(page.content, "html.parser")
+results = soup.find(id="divAvailabilityCalenderShow")
+open_slots = results.find_all("div", class_=" ui-state-available")
+for open_slot in open_slots:
+    print(open_slot, end="\n"*2)
+
+#print(results.prettify())
+#print(results)
+print(page.text)
